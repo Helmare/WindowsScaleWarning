eb9c242c2aba3d903292ac6cf0fffc77c5bf3432
Hazdryx <hazdryx@gmail.com>
2022-05-22 06:23:43 -0500

    Reloads `watch.json` and logs scale changes.

--- WindowsScaleWarning/Program.cs
+++ WindowsScaleWarning/Program.cs
@@ -5,6 +5,8 @@ namespace WSW
 {
     internal static class Program
     {
+        private const string WatchConfigFile = "watch.json";
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -15,20 +17,61 @@ namespace WSW
             Application.SetCompatibleTextRenderingDefault(false);
             Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
 
+            Log.Info("New instance launched.");
+
             // Check if process is already running.
             if (Process.GetProcessesByName("wsw").Length > 1)
             {
-                Log.Fatal("Instance of this application already running.");
+                Log.Fatal("Previous instance already running.");
                 return;
             }
 
+            Run();
+        }
+
+        /// <summary>
+        ///     Runs the main loop.
+        /// </summary>
+        private static void Run()
+        {
+            // Setup variables.
             ProcessWatch[] watches = LoadWatches();
+            DateTime lastWrite = File.GetLastWriteTime(WatchConfigFile);
+            double lastScale = 0;
+
+            // Main Loop
             while (true)
             {
                 Thread.Sleep(250);
-                if (Screen.PrimaryScreen.GetScale() == 1) continue;
+                double scale = Screen.PrimaryScreen.GetScale();
+                if (scale != lastScale)
+                {
+                    if (scale == 1)
+                    {
+                        Log.Info("Stopping watch, scale set to 100%");
+                    }
+                    else if (lastScale == 1 || lastScale == 0)
+                    {
+                        Log.Info($"Starting watch, scale set to {scale * 100:N0}%");
+                    }
+                    else
+                    {
+                        Log.Debug($"Windows scale changed to {scale * 100:N0}%");
+                    }
+                    lastScale = scale;
+                }
+                if (scale == 1) continue;
 
-                // Reset watches
+                // Check if watch.json was updated
+                DateTime curWrite = File.GetLastWriteTime(WatchConfigFile);
+                if (lastWrite != curWrite)
+                {
+                    Log.Info($"{WatchConfigFile} has been updated. Reloading...");
+                    watches = LoadWatches();
+                    lastWrite = curWrite;
+                }
+
+                // Check for process open status change.
                 foreach (ProcessWatch watch in watches)
                 {
                     Process? process = Process.GetProcessesByName(watch.ProcessName).FirstOrDefault();
@@ -45,7 +88,7 @@ namespace WSW
                         if (watch.AutoKill) process.Kill();
                         if (!watch.Opened)
                         {
-                            Log.Info($"{watch.Name} opened.");
+                            Log.Info($"{watch.Name} opened, show warning.");
                             WarningBox.Show(process, watch);
                         }
                         watch.Opened = true;
@@ -60,13 +103,13 @@ namespace WSW
         /// <returns></returns>
         private static ProcessWatch[] LoadWatches()
         {
-            if (!File.Exists("watch.json"))
+            if (!File.Exists(WatchConfigFile))
             {
-                Log.Warning("watch.json file does not exist.");
+                Log.Warning($"{WatchConfigFile} file does not exist.");
                 return Array.Empty<ProcessWatch>();
             }
 
-            string json = File.ReadAllText("watch.json");
+            string json = File.ReadAllText(WatchConfigFile);
             ProcessWatch[]? watches = null;
             try
             {
@@ -74,21 +117,21 @@ namespace WSW
             }
             catch (JsonSerializationException ex)
             {
-                Log.Error($"watch.json -> {ex.Message}");
+                Log.Error($"{WatchConfigFile} -> {ex.Message}");
             }
 
             if (watches == null)
             {
-                Log.Warning("Could not load watch.json file.");
+                Log.Warning($"Could not load {WatchConfigFile} file.");
                 return Array.Empty<ProcessWatch>();
             }
             else
             {
-                Log.Info("Succsessfully loaded watch.json.");
+                Log.Info($"Succsessfully loaded {WatchConfigFile}.");
                 Log.Info($"{watches.Length} watches found.");
                 foreach (ProcessWatch watch in watches)
                 {
-                    Log.Info($"\t{watch.Name}: {watch.ProcessName}");
+                    Log.Info($"    {watch.Name}: {watch.ProcessName}");
                 }
                 return watches;
             }
